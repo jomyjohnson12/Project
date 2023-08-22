f2f0eade9cce004ef7951cde508c1232f87e7536
jomyjohnson12 <jomyjohnson12@gmail.com>
2023-08-23 03:26:07 +0530

    added

---
+++ travelproject/travelapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Place(models.Model):
+    Name=models.CharField(max_length=250)
+    img=models.ImageField(upload_to='pics')
+    desc=models.TextField()
+
+    def __str__(self):
+        return self.Name
+    
+class Team(models.Model):
+    Name=models.CharField(max_length=250)
+    img=models.ImageField(upload_to='pics')
+    desc=models.TextField()
+
+    def __str__(self):
+        return self.Name    
+    
